802dff9642d3eff5850040eb9229b624a4e82fdb
gautam123-sachin <sachingautam6239@gmail.com>
2023-12-15 16:26:33 +0530

    new setup

---
+++ src/Pages/Header/Header.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+// import { NavLink } from "react-router-dom";
+// import useAuth from "../../Hooks/useAuth";
+import logo from "../../Images/logo.png";
+
+const Header = () => {
+  // const { user, logOut } = useAuth();
+
+  // const activeStyle = {
+  //   fontWeight: "bold",
+  //   color: "#0409a9",
+  // };
+
+  return (
+    <>
+      <Navbar bg="light" variant="light" expand="lg" sticky="top">
+        <Container varient="light">
+          <Navbar.Brand>
+            <img src={logo} alt="Healthcare" width="180" height="50" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mx-auto fs-5 fw-bold">
+              <Nav.Link>Home</Nav.Link>
+              <Nav.Link>About</Nav.Link>
+              <Nav.Link>Services</Nav.Link>
+              <Nav.Link>Appointment</Nav.Link>
+            </Nav>
+            <Navbar.Text className="fs-5 fw-bold">
+              {false ? (
+                <div>
+                  <i className="fas fa-user"></i>
+                  <button
+                    // onClick={logOut}
+                    className="btn btn-secondary text-center ms-2"
+                  >
+                    <i className="fas fa-sign-out-alt mx-auto"></i>
+                  </button>
+                </div>
+              ) : (
+                <Nav.Link>
+                  <i className="fas fa-sign-in-alt me-2"></i>
+                  Login
+                </Nav.Link>
+              )}
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Header;
