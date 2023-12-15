beec433e0ea027790b57d4860a647c9b4d0a35fe
gautam123-sachin <sachingautam6239@gmail.com>
2023-12-15 16:39:15 +0530

    new Update

--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
-// import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 // import useAuth from "../../Hooks/useAuth";
 import logo from "../../Images/logo.png";
 
 const Header = () => {
   // const { user, logOut } = useAuth();
 
-  // const activeStyle = {
-  //   fontWeight: "bold",
-  //   color: "#0409a9",
-  // };
+  const activeStyle = {
+    fontWeight: "bold",
+    color: "#0409a9",
+  };
 
   return (
     <>
@@ -22,15 +22,27 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mx-auto fs-5 fw-bold">
-              <Nav.Link>Home</Nav.Link>
-              <Nav.Link>About</Nav.Link>
-              <Nav.Link>Services</Nav.Link>
-              <Nav.Link>Appointment</Nav.Link>
+              <Nav.Link activeStyle={activeStyle} as={NavLink} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link activeStyle={activeStyle} as={NavLink} to="/about">
+                About
+              </Nav.Link>
+              <Nav.Link activeStyle={activeStyle} as={NavLink} to="/services">
+                Services
+              </Nav.Link>
+              <Nav.Link
+                activeStyle={activeStyle}
+                as={NavLink}
+                to="/appointment"
+              >
+                Appointment
+              </Nav.Link>
             </Nav>
             <Navbar.Text className="fs-5 fw-bold">
               {false ? (
                 <div>
-                  <i className="fas fa-user"></i>
+                  <i className="fas fa-user"></i> Sachin
                   <button
                     // onClick={logOut}
                     className="btn btn-secondary text-center ms-2"
